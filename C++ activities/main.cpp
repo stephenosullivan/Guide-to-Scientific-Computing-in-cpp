c3ee04418449253e485c90c75105951273518f3d
Stephen O'Sullivan <osullisg@gmail.com>
2015-08-01 23:07:35 -0700

    Initial Commit

---
+++ C++ activities/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ activities
+//
+//  Created by Stephen O'Sullivan on 8/1/15.
+//  Copyright (c) 2015 Stephen O'Sullivan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
