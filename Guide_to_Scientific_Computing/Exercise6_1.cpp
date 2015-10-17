86ba5a335ec0a655bfdf97aadd15b93e9186a43c
Stephen O'Sullivan <osullisg@gmail.com>
2015-10-16 20:50:51 -0700

    ch5

---
+++ Guide_to_Scientific_Computing/Exercise6_1.cpp
@@ -0,0 +1,13 @@
+//
+//  Exercise6_1.cpp
+//  Guide to Scientific Computing
+//
+//  Created by Stephen O'Sullivan on 10/16/15.
+//  Copyright (c) 2015 Stephen O'Sullivan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    return 0;
+}
